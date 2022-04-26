ac11162a7ab04dcc5c518d0a4ef0bd6f790e7943
Mark <markwilkins86@gmail.com>
2022-04-26 13:38:09 -0400

    commit tree b0e520e9ce47237f01f1fd9f20c2cf4b2e931153

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const { createStubInstance } = require("sinon/lib/sinon/stub");
+
 // Write your solution in this file!
+var customerName = 'bob';
+function name() {
+}
+customerName();
+
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase();
+     return customerName;
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+//let leastFavoriteCustomer = 'Rick'
+//function changeLeastFavoriteCustomer() {
+//    leastFavoriteCustomer = 'Kyle';
+//}
+
+const leastFavoriteCustomer = "d";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "f";
+}
